2e552370423d4e8d352fd8b2162de3352ae4583b
Dave Stewart <dave@davestewart.co.uk>
2018-02-11 19:57:37 +0000

    Updated build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,44 @@
-import resolve from 'rollup-plugin-node-resolve'
+// ------------------------------------------------------------------------------------------
+// setup
+// ------------------------------------------------------------------------------------------
+
 import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
+import uglify from 'rollup-plugin-uglify';
+import buble from 'rollup-plugin-buble';
 
-const external = Object.keys(require('./package.json').dependencies || {});
+const pkg = require('./package.json')
+const external = Object.keys(pkg.dependencies || {});
 
-export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/browser.js',
-    external: external,
-    output: {
-      globals: {
-        vue: 'Vue',
-      },
-      name: 'VueSource',
-      file: pkg.browser['index.js'],
-      format: 'umd',
-    },
-    plugins: [
-      resolve(),  // so Rollup can find `vue`
-      commonjs(), // so Rollup can convert `vue` to an ES module
-      babel({
-        exclude: 'node_modules/**'
-      }),
-    ]
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/vue-source.js',
-    external: external,
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
-    ]
-
+function output (file, format = 'umd') {
+  return {
+    name: pkg.name.replace(/(^\w|-\w)/g, c => c.replace('-', '').toUpperCase()),
+    file: 'dist/' + file,
+    format: format,
+    exports: 'default',
   }
-];
+}
+
+// ------------------------------------------------------------------------------------------
+// build
+// ------------------------------------------------------------------------------------------
+
+const umd = {
+  input: 'src/main.js',
+  external: external,
+  output: output('bundle.js'),
+  plugins: [
+    commonjs(),
+    buble()
+  ]
+}
+
+const min = Object.assign({}, umd, {
+  output: output('bundle.min.js'),
+  plugins: [...umd.plugins, uglify()]
+})
+
+const es = Object.assign({}, umd, {
+  output: output('bundle.es.js', 'es')
+})
+
+export default [umd, min, es]
--- src/browser.js
+++
@@ -1,4 +0,0 @@
-import plugin from './vue-source'
-
-export default plugin.install
-plugin.install.defaults = plugin.defaults
